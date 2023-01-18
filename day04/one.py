95a8305f06d97c802433c2bc5080825df16c34e7
Joost Lek <joost@joostlek.nl>
2023-01-18 15:18:30 +0100

    day 04 one clean up code

--- day04/one.py
+++ day04/one.py
@@ -1,21 +1,15 @@
 def pairs(filename):
     with open(filename) as fileh:
         for line in fileh:
-            if line.strip():
-                one, two = line.strip().split(',', 1)
-                yield tuple(map(int, one.split('-'))), tuple(map(int, two.split('-')))
+            one, two = line.strip().split(',', 1)
+            yield tuple(map(int, one.split('-'))), tuple(map(int, two.split('-')))
 
 def fully_contains(one, two):
-    if one[0] > two[0]:  # always make 1 start first
-        one, two = two, one
-    if one[1] < two[0]:  # there is no overlap at all
-        return False
-    if one[1] >= two[1] or one[0] == two[0]:  # there is overlap
-        return True
+    return one[0] >= two[0] and one[1] <= two[1]
 
 def full_contains_count(filename):
     for one, two in pairs(filename):
-        if fully_contains(one, two):
+        if fully_contains(one, two) or fully_contains(two, one):
             yield 1
 
 if __name__ == "__main__":
